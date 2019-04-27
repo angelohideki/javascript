69c41a970a8d60000e093258c53bf337215e8812
Angelo Hideki Noda <angelonoda@gmail.com>
2019-04-27 20:17:49 -0300

    Evento click

---
+++ calculator/scripts/controller/CalcController.js
@@ -0,0 +1,75 @@
+class CalcController{
+
+    constructor(){
+
+        this.locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this.currentDate;
+        this.initialize();
+        this.initButtonsEvents();
+    }
+
+    initialize(){
+
+        this.setDisplayDateTime();
+
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
+
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn,index)=>{
+            btn.addEventListener('click', e=>{
+                console.log(btn);
+            });
+        });
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this.locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+}
