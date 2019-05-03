1aed177afdc0145e7bb84c769e8010859c594f2b
Angelo Hideki Noda <angelonoda@gmail.com>
2019-05-02 21:59:41 -0300

    fim calculadora

--- calculator/scripts/controller/CalcController.js
+++ calculator/scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController{
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = true;
         this._lastOperator = "";
         this._lastNumber = "";
 
@@ -55,11 +57,39 @@ class CalcController{
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dbclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            this.playAudio();
 
             switch (e.key) {
 
@@ -169,7 +199,17 @@ class CalcController{
 
     getResult(){
 
-        return eval(this._operation.join(""));
+        try{
+
+            return eval(this._operation.join(""));
+            
+        }catch(e){
+            setTimeout(()=>{
+                this.setError();
+            },1);
+
+        }
+
     }
 
     calc(){
@@ -308,6 +348,8 @@ class CalcController{
 
     execBtn(value){
 
+        this.playAudio();
+
         switch (value) {
 
             case 'ac':
@@ -419,6 +461,11 @@ class CalcController{
     }
 
     set displayCalc(value){
+
+        if(value.toString().length > 10){
+            this.setError();
+            return false;
+        }
         this._displayCalcEl.innerHTML = value;
     }
 
